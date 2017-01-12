87c460895c627fcdcd2729c823a7654fd9daa8f4
Meredith Durbin <meredith.durbin@gmail.com>
2017-01-11 20:32:36 -0800

    add address validator

--- utils/form.py
+++ utils/form.py
@@ -1,13 +1,24 @@
 from wtforms import Form, validators
 from wtforms.fields import RadioField, SelectField, StringField, TextAreaField
 
+from wtforms.validators import ValidationError
+
+from utils import query_for_reps
+
+def address_check(form, field):
+    try:
+        query_for_reps(field.data)
+    except:
+        raise ValidationError('Your address could not be processed. Sorry.')
+
 class ContactForm(Form):
     name    = StringField('Name', [validators.DataRequired()], render_kw={"placeholder": "Brett Morris"})
     title   = SelectField('Title (optional)', choices=[('', ''), ('Dr.', 'Dr.'), ('Ms.', 'Ms.'), ('Mrs.', 'Mrs.'),
                                                        ('Mr.', 'Mr.'), ('Mx.', 'Mx.')],
                           default='')
     affil   = StringField('Affiliation (optional)', render_kw={"placeholder": "University of Washington"})
-    address = StringField('Address', [validators.DataRequired()], render_kw={"placeholder": "60 Garden St, Cambridge MA 02138"})
+    address = StringField('Address', [validators.DataRequired(), address_check],
+                          render_kw={"placeholder": "60 Garden St, Cambridge MA 02138"})
     stance  = RadioField('Stance', [validators.DataRequired()], choices=[('support', 'support'), ('oppose', 'oppose')],
                          default='support', render_kw={"style": "list-style-type:none; display:inline;"})
     subject = StringField('Subject', [validators.DataRequired()], render_kw={"placeholder": "House Bill 1234"})
